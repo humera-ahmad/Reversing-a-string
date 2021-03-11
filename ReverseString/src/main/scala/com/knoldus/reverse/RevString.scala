f10578e7fabe0554719864b25ec129041e651cc9
Humera <76787291+humera-ahmad@users.noreply.github.com>
2021-03-11 20:16:36 +0530

    Update RevString.scala

--- ReverseString/src/main/scala/com/knoldus/reverse/RevString.scala
+++ ReverseString/src/main/scala/com/knoldus/reverse/RevString.scala
@@ -1,7 +1,9 @@
 package com.knoldus.reverse
 
+import scala.collection.immutable.StringOps
+
 class RevString {
-  def reversing(s: String) : String = {
+  def reversing(s: StringOps) : StringOps = {
     for(i <- s.length - 1 to 0 by -1) yield s(i)
   }.mkString
 }
